1db7f03328dcdd49c60dc13eb6be8bf6aad8c477
Weuller Marcos <weullermarcos@MacBook-Pro-de-Weuller.local>
2016-12-26 20:24:17 -0200

    Criando tela para comentarios

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -4,22 +4,19 @@ import java.util.List;
 
 import models.Post;
 import play.Play;
+import play.data.validation.Required;
 import play.mvc.Before;
 import play.mvc.Controller;
 
 public class Application extends Controller {
 
-//	Original Index method
-//    public static void index() {
-//    	
-//        render();
-//    }
 	
 	@Before
 	static void addDefaults() {
 	    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
 	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
 	}
+	
 	
 	public static void index() {
 		
@@ -31,5 +28,21 @@ public class Application extends Controller {
         
         render(frontPost, olderPosts);
     }
+	
+	
+	public static void show(Long id) {
+	    Post post = Post.findById(id);
+	    render(post);
+	}
+	
+	public static void postComment(Long postId, @Required String author, @Required String content) {
+	    Post post = Post.findById(postId);
+	    if(validation.hasErrors()) {
+	        render("Application/show.html", post);
+	    }
+	    post.addComment(author, content);
+	    flash.success("Thanks for posting %s", author);
+	    show(postId);
+	}
 
 }
--- yabe/app/models/Post.java
+++ yabe/app/models/Post.java
@@ -57,6 +57,15 @@ public class Post extends Model {
 	    return this;
 	}
 	
+	//Adiciona paginacao
+	public Post previous() {
+	    return Post.find("postedAt < ? order by postedAt desc", postedAt).first();
+	}
+	 
+	public Post next() {
+	    return Post.find("postedAt > ? order by postedAt asc", postedAt).first();
+	}
+	
 }
 
 
